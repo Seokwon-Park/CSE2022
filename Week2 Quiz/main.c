7c7e75ab78c5ce359ba57ac9b993e7270fd9567e
kaiz711 <kaiz711@naver.com>
2016-09-12 16:03:23 +0900

    Week2 Quiz

---
+++ Week2 Quiz/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+double randnum(const double min, const double max)
+{
+	double temp = (double)rand() / (double)RAND_MAX;
+
+	temp = min + (max - min)*temp;
+
+	return temp;
+}
+
+bool chkcir(const double x, const double y)
+{
+	const double x_c = 1.5;
+	const double y_c = 0.5;
+	const double r = 0.5;
+
+	const double f = (x - x_c)*(x - x_c) / 9 + (y - y_c)*(y - y_c) - r*r;
+
+
+	if (f > 0.0)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+//bool chk_square(const double x, const double y)
+//{
+//	if (y > 0.25 && y < 0.75 && x< 2.5 && x >0.5)
+//	{
+//		return true;
+//	}
+//	else
+//	{
+//		return false;
+//	}
+//}
+
+void main()
+{
+	FILE *of = fopen("quiz.txt", "w");
+
+	srand((unsigned int)time(NULL));
+
+	for (int i = 0; i < 100000; i++)
+	{
+		double x = randnum(0.0, 3.0);
+		double y = randnum(0.0, 3.0);
+
+		if (chkcir(x, y) == true)
+		{
+			fprintf(of, "%f %f\n", x, y);
+			//fprintf(of, "%f %f\n", x+2.0, y);
+		}
+		//else if (chk_square(x,y) == true)
+		//{
+		//	fprintf(of, "%f %f\n", x, y);
+		//}
+	}
+	fclose(of);
+}
